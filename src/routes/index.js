f607d02e7034df82576f3040fb27e940a90c8d6e
UnActimel <trnikanthony@gmail.com>
2023-12-01 12:07:04 +0100

    ajout annonces basiques et modif filtres

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
 
 router.get("/", (_, res) => {
-  res.render("pages/index", { loggedIn: false });
+  
+  let annonces = [
+    {
+      seller: "Jean Mahmoud",
+      objectName: "Livre de math",
+      price: 11,
+      state: "Nul à chier"
+    },
+    {
+      seller: "Abdul Kader",
+      objectName: "Livre d'anglais",
+      price: 40,
+      state: "Très bon"
+    }
+  ];
+  res.render("pages/index", { loggedIn: false, annonces });
 });
 
 module.exports = router;
