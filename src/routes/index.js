847392b88e5029850dfd646b41cc50f46cb42127
Frivox1 <mertens.valery@gmail.com>
2023-12-04 13:38:56 +0100

    page profil finie + responsive

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,41 @@ router.get("/", (_, res) => {
   res.render("index");
 });
 
+router.get("/profile", (_, res) => {
+
+  let annonces = [
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Jean Mahmoud",
+      objectName: "Livre de math",
+      price: 11,
+      state: "Nul à chier"
+    },
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Abdul Kader",
+      objectName: "Livre d'anglais",
+      price: 40,
+      state: "Très bon"
+    },
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Max Günter",
+      objectName: "Livre d'allemand",
+      price: 35,
+      state: "Bon"
+    },
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Olaf Müller",
+      objectName: "Livre d'histoire",
+      price: 65,
+      state: "Très bon"
+    }
+  ];
+
+
+  res.render("pages/profile", { loggedIn: false, annonces });
+});
+
 module.exports = router;
