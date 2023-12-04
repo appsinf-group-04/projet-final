dd74670f3b2ae565c9e843771cd8f54bb6de0413
Mertens Valéry <73830772+Frivox1@users.noreply.github.com>
2023-12-04 14:22:25 +0100

    Profile page (#16)
    * page profil finie + responsive
    
    * rajout du bouton création
    
    * style: run linter

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,39 @@ router.get("/", (_, res) => {
   res.render("index");
 });
 
+router.get("/profile", (_, res) => {
+  const annonces = [
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Jean Mahmoud",
+      objectName: "Livre de math",
+      price: 11,
+      state: "Nul à chier",
+    },
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Abdul Kader",
+      objectName: "Livre d'anglais",
+      price: 40,
+      state: "Très bon",
+    },
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Max Günter",
+      objectName: "Livre d'allemand",
+      price: 35,
+      state: "Bon",
+    },
+    {
+      img: "/public/images/book_placeholder.png",
+      seller: "Olaf Müller",
+      objectName: "Livre d'histoire",
+      price: 65,
+      state: "Très bon",
+    },
+  ];
+
+  res.render("pages/profile", { loggedIn: false, annonces });
+});
+
 module.exports = router;
