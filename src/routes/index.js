633d258cd2769f87e8eaec76d711eb4ba6a24cc8
Actimel <trnikanthony@gmail.com>
2023-12-04 13:27:08 +0100

    test responsive page principale

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ router.get("/", (_, res) => {
   let annonces = [
     {
       seller: "Jean Mahmoud",
+      sellerRating: 2.11,
       objectName: "Livre de math",
       price: 11,
       state: "Nul à chier"
     },
     {
       seller: "Abdul Kader",
+      sellerRating: 4,
       objectName: "Livre d'anglais",
       price: 40,
       state: "Très bon"
