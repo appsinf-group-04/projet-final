da5c13f7fa22be346845613931e3ada461c42c3d
Actimel <trnikanthony@gmail.com>
2023-12-10 22:27:46 +0100

    fix connect db pour windows =)

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.listen(process.env.PORT || 8080, () => {
   console.log("Listening at http://localhost:8080");
 
   mongoose
-    .connect("mongodb://localhost:27017/projet-final")
+    .connect("mongodb://127.0.0.1:27017/projet-final")
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
 });
