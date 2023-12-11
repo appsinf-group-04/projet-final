b6d2f875636b0eac72666ccd19613dda192a4c9d
Anthony <113705707+UnActimel@users.noreply.github.com>
2023-12-11 20:03:58 +0100

    page index (#23)
    * fix connect db pour windows =)
    
    * filtres index style et responsive
    
    * style moche
    
    * style: run linter
    
    ---------
    
    Co-authored-by: NotAJocke <jockerider199@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,16 @@ if (showRegisterModal) {
     registerModal.close();
   });
 }
+
+// Toggle the visibility of the filter section
+document.addEventListener("DOMContentLoaded", function() {
+  const filtresSection = document.getElementById("filtresSection");
+  const toggleFiltresButton = document.getElementById("toggleFiltresButton");
+  const announcesSection = document.getElementById("announcesSection");
+
+  toggleFiltresButton.addEventListener("click", function() {
+    filtresSection.classList.toggle("hidden");
+    filtresSection.classList.toggle("w-3/5")
+    announcesSection.classList.toggle("hidden")
+  });
+});
