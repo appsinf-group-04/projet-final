ed60164679695b7efdd1ae0d74a481fe76a0066d
NotAJocke <jockerider199@gmail.com>
2023-12-19 14:41:16 +0100

    feat: add more admin graphs

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
 
+const PostModel = require("./models/post");
 const app = express();
 
 app
@@ -25,5 +26,16 @@ app.listen(process.env.PORT || 8080, () => {
   mongoose
     .connect("mongodb://127.0.0.1:27017/projet-final")
     .then(() => console.log("Connected to MongoDB"))
+    .then(
+      async () =>
+        await new PostModel({
+          title: "Hello World",
+          price: 10,
+          state: "Good",
+          description: "This is a description",
+          createdAt: Date.now(),
+          refUser: "6578c44d59fd97756646677d",
+        }),
+    )
     .catch((err) => console.log(err));
 });
