011a05e8120f7c26895aace9ee0f73ea597f0268
NotAJocke <jockerider199@gmail.com>
2023-12-20 13:30:26 +0100

    fix: remove test

--- src/server.js
+++ src/server.js
@@ -27,8 +27,5 @@ app.listen(process.env.PORT || 8080, () => {
   mongoose
     .connect("mongodb://127.0.0.1:27017/projet-final")
     .then(() => console.log("Connected to MongoDB"))
-    .then(async () => {
-      const post = await getPost("6581bde93e21d2fd5d6e5380");
-    })
     .catch((err) => console.log(err));
 });
