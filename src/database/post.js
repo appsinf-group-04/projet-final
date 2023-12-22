dfd2f1a52a96295359e0034353309c7b5044a684
Anthony <113705707+UnActimel@users.noreply.github.com>
2023-12-22 16:16:37 +0100

    ajout avis utilisateurs & ban users (#52)
    * Ajout de la page produit à developement (#22)
    
    * Profile page (#16)
    
    * page profil finie + responsive
    
    * rajout du bouton création
    
    * style: run linter
    
    * fix: structure
    
    * avancement de la page detail du produit
    
    * avancement de la page produit
    
    * Page de produit finie
    
    * petits changements sur page produit
    
    * page register et login (#26)
    
    * page register et login simple et efficace finie
    
    * changement du css
    
    * déplacement des erreurs sous les inputs
    
    * feat: admin graphs (2) (#25)
    
    - Accounts/bans over time
    - Accounts created by day
    
    * page index (#23)
    
    * fix connect db pour windows =)
    
    * filtres index style et responsive
    
    * style moche
    
    * style: run linter
    
    ---------
    
    Co-authored-by: NotAJocke <jockerider199@gmail.com>
    
    * ui: dash bans table (#27)
    
    - Latest bans table
    - Search bans
    - Unban system
    
    * feat: logins by day graph
    
    * Val (#30)
    
    * fix: details admin btns (#31)
    
    * Posts model & ice (#32)
    
    * fix: user not defined sur page register
    
    * feat: add admin route to header and header to admin page
    
    * feat: add more admin graphs (#36)
    
    * feat: add posts db populate helper (#37)
    
    * refactor: dashboard responsive and style (#38)
    
    * refactor: uniformisation du style (#39)
    
    * post system (#34)
    
    * ajout post a la db temporaire
    
    * ajout des annonces dans la db + affichage sur index
    
    * refactoring posts code
    
    * affichage des nouvelles annonces en premier sur index
    
    * ajout min max prix annonce
    
    * fix crash avec /profile
    
    * fix index page UI bug
    
    * ajout fonction getOnePost avec un id de post
    
    * fix: rework database functions and model
    
    * feat: add errors on post creation
    
    * fix: main page post display
    
    * feat: post images upload
    
    * style: run linter
    
    ---------
    
    Co-authored-by: NotAJocke <jockerider199@gmail.com>
    
    * refactor: suppression de la pp sur la page profil
    
    * fix: remove test
    
    * fix UI bug <a> index
    
    * placeholder form annonce + fix padding
    
    * lien entre index et details + annonces sur page profil (#42)
    
    * lien entre la page index et la page details
    
    * rajout des annonces sur page profil
    
    * annonces clickable dans profile
    
    * passage de EN à FR
    
    * faute d'ortho rectifié
    
    * fix: affichage des images des annonces sur page profil
    
    * rajout d'une simple page d'erreur 404 (#46)
    
    * Ban user (#47)
    
    * ajout du ban user depuis une annonce
    
    * ajout commentaires et espaces dans le code
    
    * pareil
    
    * bouton supprimer annonce
    
    * ajout message confirmation avant suppression de l'annonce
    
    * style: run linter
    
    * retirer description annonces page index
    
    ---------
    
    Co-authored-by: Frivox1 <mertens.valery@gmail.com>
    
    * corrections en -> fr
    
    * ajout attribution des notes
    
    * style: run linter
    
    * fixes
    
    ---------
    
    Co-authored-by: Mertens Valéry <73830772+Frivox1@users.noreply.github.com>
    Co-authored-by: Jocke <jockerider199@gmail.com>
    Co-authored-by: Frivox1 <mertens.valery@gmail.com>

--- src/database/post.js
+++ src/database/post.js
@@ -67,7 +67,6 @@ async function getPost(postID) {
 }
 
 async function getPostForUser(userID) {
-  // Quelle est la diff entre find et findById
   const posts = await PostModel.find({ refUser: userID }).sort({
     createdAt: -1,
   });
@@ -83,7 +82,6 @@ async function setPictures(postID, pictures) {
 }
 
 async function deletePost(postID) {
-  console.log(postID);
   try {
     await PostModel.findByIdAndDelete(postID);
   } catch (error) {
